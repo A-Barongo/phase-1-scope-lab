3f8b0a2cc52ea0284274881a6d9d3f848f9e40be
Allan <allanbarongo5@gmail.com>
2025-03-15 08:21:04 +0300

    Completed scope demonstration lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+const leastFavoriteCustomer='sponge'
+var customerName='bob'
+
+function upperCaseCustomerName(){
+   return (customerName=customerName.toUpperCase())
+}
+
+function setBestCustomer(){
+    bestCustomer='not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='squidward'
+}
